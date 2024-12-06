b1ff1ae9e0287b880226e1e2af5a2fbd8869a48c
tushsynechron <50320377+tushsynechron@users.noreply.github.com>
2024-12-06 17:29:20 +0530

    Create Testing.java

---
+++ src/main/java/org/example/Testing.java
@@ -0,0 +1,18 @@
+public class Testing {
+
+    public static void main(String[] args) {
+
+        double n1 = -4.5, n2 = 3.9, n3 = 2.5;
+        String cust_dob="";
+        String cust_ADD="";
+
+        if( n1 >= n2 && n1 >= n3)
+            System.out.println(n1 + " is the largest number.");
+
+        else if (n2 >= n1 && n2 >= n3)
+            System.out.println(n2 + " is the largest number.");
+
+        else
+            System.out.println(n3 + " is the largest number.");
+    }
+}
