393ba0f1daba9ed3beac16ace46dd936d37eaba4
tushsynechron <50320377+tushsynechron@users.noreply.github.com>
2024-12-09 18:09:35 +0530

    Update TestClass.java

--- src/main/java/org/example/TestClass.java
+++ src/main/java/org/example/TestClass.java
@@ -2,9 +2,9 @@ package org.example;
 
 public class TestClass {
 
-  String endpoints = "http://app.zensar.com/getUserDetails";
+  String endpoints = "http://app.zensar.com/getUserDetails/cust_add";
 
-  String request = "";
+  String request = "cust_name";
 
-  String response = "Hi Tushar";
+  String response = "card_num";
 }
